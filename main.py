3d603afff993f1a9ae35e712068224ce2c5f55b3
jyw8007 <109638742+jyw8007@users.noreply.github.com>
2022-07-21 11:01:18 +0900

    anyjhgtgr

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+url2 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
+url3 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Amumu.json'
+
+
+res = requests.get(url).json()
+res2 = requests.get(url2).json()
+res3 = requests.get(url3).json()
+res4 = requests.get(url3).json()
+
+
+champ_lst = (res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
+champ_lst2 = (res['data']['Ahri']['key'], res['data']['Ahri']['name'])
+champ_lst3 = (res['data']['Amumu']['key'], res['data']['Amumu']['name'])
