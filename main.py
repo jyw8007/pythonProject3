6f25b813557f3a0b9e057ef8762a44e8f33aa992
jyw8007 <109638742+jyw8007@users.noreply.github.com>
2022-07-21 11:04:34 +0900

    test2

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import requests
 url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
 url2 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
 url3 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Amumu.json'
-
+url4 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Zeri.json'
 
 res = requests.get(url).json()
 res2 = requests.get(url2).json()
 res3 = requests.get(url3).json()
-res4 = requests.get(url3).json()
+res4 = requests.get(url4).json()
 
 
 champ_lst = (res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
